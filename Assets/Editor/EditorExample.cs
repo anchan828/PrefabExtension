f35f5721d445119a166b2ac106776cb89e0a04ee
Keigo Ando <anchan828@gmail.com>
2014-02-11 20:41:17 +0900

    first commit

---
+++ Assets/Editor/EditorExample.cs
@@ -0,0 +1,10 @@
+﻿using UnityEditor;
+
+public class EditorExample
+{
+	[MenuItem("PrefabExtension/ApplyPrefabState")]
+	static void ApplyPrefabState ()
+	{
+		Selection.gameObjects.ApplyPrefabState (false);
+	}
+}
